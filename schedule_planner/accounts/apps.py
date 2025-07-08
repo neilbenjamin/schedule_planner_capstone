42cb2e16aecd26c31b5fb422e1e7fb37f2c49fe1
neilbenjamin <neilbenjaminmusic@gmail.com>
2025-07-08 19:27:48 +0200

    added docs to apps.pf

--- schedule_planner/accounts/apps.py
+++ schedule_planner/accounts/apps.py
@@ -2,5 +2,15 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """
+    Configuration for the accounts django application and defines the
+    metadata and settings for the 'accounts' app, allowing django to access
+    and manage it.
+
+    :param default_auto_field: Specifies the default PK field type for the
+                               models in this app to BigAutoField
+    :param name: The python path to this app, used by Django for referencing
+
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
